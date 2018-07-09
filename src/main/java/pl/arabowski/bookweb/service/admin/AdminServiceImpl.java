53d67a935057d7b82d8831ceee55e3041d6a2212
Adrian Rabowski <adrian.rabowski@gmail.com>
2018-07-09 19:22:03 +0200

    added admin service implementation

---
+++ src/main/java/pl/arabowski/bookweb/service/admin/AdminServiceImpl.java
@@ -0,0 +1,63 @@
+package pl.arabowski.bookweb.service.admin;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ModelAndView;
+
+@Service
+public class AdminServiceImpl implements AdminService{
+
+	@Override
+	public ModelAndView deleteBook(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ModelAndView editAuthor(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ModelAndView deleteAuthor(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ModelAndView editPublisher(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ModelAndView deletePublisher(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addAdminRights(long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeAdminRights(long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ModelAndView editUser(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ModelAndView deleteUser(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
